072d35b35d6511db99b16ac6af91a1efdf850e22
Quirk0o <beata@obrok.eu>
2016-05-24 21:40:24 +0200

    Update main controller and create socket connection for notes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,39 +1,26 @@
 'use strict';
 
 export default class MainController {
-
-  constructor($http, $scope, Auth, socket, Note) {
-    this.$http = $http;
+  constructor($scope, Auth, socket, Note) {
     this.Auth = Auth;
     this.Note = Note;
-    this.awesomeThings = [];
-    this.notes = Note.query();
-    this.note = "";
+    this.note = '';
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
+    Note.query().$promise
+      .then(data => {
+        this.notes = data;
+        socket.syncUpdates('note', this.notes);
+      });
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('note');
     });
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
   }
 
   submitNote(note) {
     let oldNotes = this.notes;
     this.notes = oldNotes.concat([note]);
+    
     this.Note.save({
       title: 'A Note',
       content: note,
