b69a05c626ea6299b12653a3c53fc3364e8e43c8
Quirk0o <beata@obrok.eu>
2016-06-03 09:11:03 +0200

    Update main controller with handlers and enable editing existing notes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,7 +4,8 @@ export default class MainController {
   constructor($scope, Auth, socket, Note) {
     this.Auth = Auth;
     this.Note = Note;
-    this.note = '';
+    this.notes = [];
+    this.note = { content: '', title: '', tags: [] };
 
     Note.query().$promise
       .then(data => {
@@ -20,12 +21,39 @@ export default class MainController {
   submitNote(note) {
     note.author = this.Auth.getCurrentUser()._id;
 
-    let oldNotes = this.notes;
-    this.notes = oldNotes.concat([note]);
+    if (this.note._id) {
+      this.Note.update({ id: this.note._id }, note).$promise
+        .then(data => {
+          this.note = data;
+          this.notes = this.notes.map(elem => {
+            if (elem._id === data._id) return data;
+            else return elem;
+          })
+        });
+    } else {
 
-    this.Note.save(note).$promise
-      .then(data => this.notes.filter((elem) => elem !== note))
-      .catch(err => this.notes = oldNotes);
-    this.note = '';
+      let oldNotes = this.notes;
+      this.notes = oldNotes.concat([note]);
+
+      this.Note.save(note).$promise
+        .then(data => {
+          this.note = data;
+          this.notes = oldNotes.concat([data])
+        })
+        .catch(() => this.notes = oldNotes);
+    }
+  }
+
+  handleNoteSelection(note) {
+    this.Note.get({id: note._id}).$promise
+      .then(data => this.note = data);
+  }
+
+  handleNoteDelete(note) {
+    let oldNotes = this.notes;
+    this.notes = oldNotes.filter(elem => elem !== note);
+
+    this.Note.delete({id: note._id}).$promise
+      .catch(() => this.notes = oldNotes);
   }
 }
